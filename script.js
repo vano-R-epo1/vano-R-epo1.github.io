34ac6661bab3ebdfc69a25564addebcd7b39deab
van_or <ronavjhureley@gmail.com>
2026-03-21 17:17:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let counter = 0;
 let answerArray = ["A","B","C","D"];
 function isItCorrect(button) {
+    counter++;
     if (answerArray[counter] === button) {
         alert("Correct");
     }
-    counter++;
 }
 
